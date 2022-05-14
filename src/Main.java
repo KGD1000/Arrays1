34686493d71a9cd65edd947cce13363818cf7960
KD <kgd1000@gmail.com>
2022-05-14 20:46:53 +0300

    В 16 строке выдает ошибку. Не пойму, почему

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+
+      int[] weights = new int[12];
+      weights[0] = 90;
+      int januaryWeight = weights[0];
+        System.out.println(januaryWeight);
+        System.out.println(weights[0]);
+        System.out.println(weights[4]);
+        int january = 0;
+        System.out.println(weights[january]);
+
+           for (int i = 0; i < 12; i++); {
+            System.out.println(weights[i]);
+        }
+
+
+    }
+}
